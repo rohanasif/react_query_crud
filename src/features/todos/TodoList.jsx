697475f85df4a73ce783f3abf58c65afe985ee7c
rohanasif1990 <rohanasif1990@gmail.com>
2023-12-09 16:55:34 +0500

    add TodoList component

---
+++ src/features/todos/TodoList.jsx
@@ -0,0 +1,57 @@
+import { useQuery, useMutation, useQueryClient } from "react-query";
+import { getTodos, addTodo, updateTodo, deleteTodo } from "../../api/todosApi";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+const TodoList = () => {
+  const [newTodo, setNewTodo] = useState("");
+  const queryClient = useQueryClient();
+  const {
+    isLoading,
+    isError,
+    error,
+    data: todos,
+  } = useQuery("todos", getTodos);
+  const addTodoMutation = useMutation(addTodo, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("todos");
+    },
+  });
+
+  const updateTodoMutation = useMutation(updateTodo, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("todos");
+    },
+  });
+
+  const deleteTodoMutation = useMutation(deleteTodo, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("todos");
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodoMutation.mutate({ userId: 1, title: newTodo, completed: false });
+    setNewTodo("");
+  };
+
+  const newItemSection = (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="new-todo">Enter a new todo item</label>
+      <div className="new-todo">
+        <input
+          type="text"
+          id="new-todo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Enter new todo"
+        />
+      </div>
+      <button className="submit">
+        <FontAwesomeIcon icon={faUpload} />
+      </button>
+    </form>
+  );
+};
